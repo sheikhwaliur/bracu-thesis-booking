69b36987486708a37b7b3657929f8e589a28c953
Sheikh Waliur Rahman <sheikhwaliur001@gmail.com>
2026-05-04 18:29:44 -0400

    fix env loading

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const authRoutes = require('./routes/auth');
 const slotRoutes = require('./routes/slots');
